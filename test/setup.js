f3cd256bbb28446b5b24ee87a8807501a665471c
Peter Schussheim <peter@schussheim.com>
2016-12-25 18:50:15 -0500

    test(Stream component): add test for Stream component

---
+++ test/setup.js
@@ -0,0 +1,21 @@
+// We are exposing a jsdom generated document and window
+// object as global objectsto be run by our tests.
+
+import React from 'react';
+import { expect } from 'chai';
+import jsdom from 'jsdom';
+
+const doc = jsdom.jsdom(`<!doctype html><html><body></body></html>`);
+const win = doc.defaultView;
+
+global.document = doc;
+global.window = win;
+
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
+global.React = React;
+global.expect = expect;
