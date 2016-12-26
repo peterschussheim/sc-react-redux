630f1c3a4a323f3db726cf05fc45f7fcc18dbb69
Peter Schussheim <peter@schussheim.com>
2016-12-26 16:09:58 -0500

    feat(browser): add mapStateToProps fn

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
 function Stream({ tracks = [] }) {
   return (
@@ -10,4 +11,14 @@ function Stream({ tracks = [] }) {
   );
 }
 
-export default Stream;
+// using the returned function of connect, we take Stream component
+// as and argument to return a HOC to access the redux store while
+// the Stream component is concerned with just presenting our data.
+function mapStateToProps(state) {
+  const tracks = state.track;
+  return {
+    tracks
+  }
+}
+
+export default connect(mapStateToProps)(Stream);
