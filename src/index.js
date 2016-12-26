6a81fa1e2545b91331e511467206bf4a0e91847b
Peter Schussheim <peter@schussheim.com>
2016-12-26 15:57:16 -0500

    feat(browser): connect redux store with all child components via index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
 import Stream from './components/Stream';
@@ -14,9 +15,11 @@ const tracks = [
 ];
 
 const store = configureStore();
-store.dispatch(action.setTracks(tracks));
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <Stream />,
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
   document.getElementById('app')
 );
