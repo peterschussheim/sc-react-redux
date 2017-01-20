af09e72454102cdd85b521dbd67fe55a5e6711e6
Peter Schussheim <peter@schussheim.com>
2017-01-20 16:58:04 -0500

    update configureStore and reducers

---
+++ src/components/App/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// App passes all children to future components
+function App({ children }) {
+  return <div>{children}</div>;
+}
+
+export default App;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 import track from './track';
 
 // using combineReducers, we can have multiple reducers,
 // then combine them so that each reducer returns a substate.
-export default combineReducers({
-  track,
-});
+export default combineReducers({ track, routing: routerReducer });
